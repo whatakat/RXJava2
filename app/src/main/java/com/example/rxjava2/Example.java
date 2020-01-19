4bf6e67e32897d8b293330c86ce3dce866b1072c
Leoneed <40763564+whatakat@users.noreply.github.com>
2020-01-19 13:18:38 +0300

    add filter func

--- app/src/main/java/com/example/rxjava2/Example.java
+++ app/src/main/java/com/example/rxjava2/Example.java
@@ -1,8 +1,12 @@
 package com.example.rxjava2;
 
 import java.io.IOException;
+import java.lang.reflect.Array;
+import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.HashSet;
 import java.util.List;
+import java.util.Set;
 
 import io.reactivex.Observable;
 import io.reactivex.ObservableEmitter;
@@ -35,6 +39,14 @@ public class Example {
             @Override
             public Integer apply(String s) throws Exception {
                 return Integer.parseInt(s)*2;
+            }
+        };
+        Function<Integer,Boolean> myFuncFilter = new Function<Integer, Boolean>() {
+            @Override
+            public Boolean apply(Integer i) throws Exception {
+                int res = i%3;
+                if (res==0) return true;
+                return false;
             }
         };
         Observable<String> observable = Observable.fromArray("1", "2", "3", "4");
