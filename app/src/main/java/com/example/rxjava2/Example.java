21c0280dd1ae5c4359a6c80e606684bda43e72bf
Leoneed <40763564+whatakat@users.noreply.github.com>
2020-01-21 00:28:24 +0300

    create annotations

--- app/src/main/java/com/example/rxjava2/Example.java
+++ app/src/main/java/com/example/rxjava2/Example.java
@@ -1,6 +1,8 @@
 package com.example.rxjava2;
 
 
+import androidx.annotation.IntDef;
+
 import io.reactivex.annotations.NonNull;
 
 
@@ -8,6 +10,7 @@ import io.reactivex.annotations.NonNull;
 public class Example {
     public void something(){
         doSomethingWithNotNull(null);//you can't put null this. @Nullable - you can
+        functionWithTwoModes(MODE1);// annotations
     }
     private void doSomethingWithNotNull(@NonNull String string){
 
@@ -17,7 +20,22 @@ public class Example {
     @ColorRes
     @Nullable
     @NotNull
+    @FloatRange - for interval value - from 1 to 5 for example
+    @IntRange
+    @RequiresPermission(value = Manifest.permission.WRITE_EXTERNAL_STORAGE) - it's good idea for method works with storage
+    @CheckResult
 
      */
+    static final int MODE1 = 0;
+    static final int MODE2 = 1;
+
+    @IntDef({MODE1,MODE2})
+    @interface Mode{}
+
+    private void functionWithTwoModes(@Mode Integer mode){
+
+    }
+
+
 
 }
